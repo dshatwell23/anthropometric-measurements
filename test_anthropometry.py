b88b77c1bf8766032cdbf16de2b71e435a302b8a
David Shatwell <43528461+dshatwell23@users.noreply.github.com>
2022-08-20 13:21:08 -0500

    Add files via upload

---
+++ test_anthropometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 21:20:07 2021
+
+@author: davidshatwell
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from anthropometry import AnthropometricModel
+
